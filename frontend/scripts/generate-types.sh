8c912be96aa0907c077e2a4932cd423cc1125b8c
“sajeremy” <9jsantiago4@gmail.com>
2025-07-20 15:34:10 -0400

    chore: move type generation outside of bundling scope

--- frontend/scripts/generate-types.sh
+++ frontend/scripts/generate-types.sh
@@ -2,4 +2,4 @@
 
 # Load .env.local if it exists
 [ -f .env.local ] && source .env.local
-openapi-typescript "$VITE_OPENAPI_URL" --output src/types/api.ts
+openapi-typescript "$VITE_OPENAPI_URL" --output types/api.ts
