10b10fa5125d80016266154d29d49f191c6bb171
“sajeremy” <9jsantiago4@gmail.com>
2025-07-20 13:27:33 -0400

    fix: vite env variable import

--- frontend/src/services/api/groceryReceipt.ts
+++ frontend/src/services/api/groceryReceipt.ts
@@ -5,11 +5,11 @@ type receiptInput = {
   imgFile: File;
 };
 
-const foodApiBaseUrl = import.meta.env.FOOD_API_BASE_URL;
+const foodApiBaseUrl = import.meta.env.VITE_FOOD_API_BASE_URL;
 
 const parseGroceryReceipt = async (inputs: receiptInput) => {
   const formData = createSnakeCaseFormData(inputs);
-  const response = await fetch(`${foodApiBaseUrl}/grocery-receipt`, {
+  const response = await fetch(`${foodApiBaseUrl}/grocery_receipt`, {
     method: "POST",
     body: formData,
   });
